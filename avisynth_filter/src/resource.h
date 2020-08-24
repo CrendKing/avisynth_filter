a423d0cf2ed3f6ec5ea92427127fde9ba25b62c8
Crend King <975235+CrendKing@users.noreply.github.com>
2020-08-24 14:15:53 -0700

    Fix line ending

--- avisynth_filter/src/resource.h
+++ avisynth_filter/src/resource.h
@@ -1,4 +1,4 @@
-﻿//{{NO_DEPENDENCIES}}
+//{{NO_DEPENDENCIES}}
 // Microsoft Visual C++ generated include file.
 // Used by avisynth_filter.rc
 //
@@ -51,12 +51,12 @@
 #define IDC_TEXT_FORMAT_VALUE             2016
 
 // Next default values for new objects
-// 
+//
 #ifdef APSTUDIO_INVOKED
 #ifndef APSTUDIO_READONLY_SYMBOLS
-#define _APS_NEXT_RESOURCE_VALUE        106
-#define _APS_NEXT_COMMAND_VALUE         40001
-#define _APS_NEXT_CONTROL_VALUE         1009
-#define _APS_NEXT_SYMED_VALUE           102
+#define _APS_NEXT_RESOURCE_VALUE          106
+#define _APS_NEXT_COMMAND_VALUE           40001
+#define _APS_NEXT_CONTROL_VALUE           1009
+#define _APS_NEXT_SYMED_VALUE             102
 #endif
 #endif
