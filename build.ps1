37f1f805baed38ad820f985e379f8604ce5bba08
Crend King <975235+CrendKing@users.noreply.github.com>
2022-02-09 17:43:42 -0800

    Make build script more flexible

--- build.ps1
+++ build.ps1
@@ -1,4 +1,8 @@
-$vsPath = & "${env:ProgramFiles(x86)}\Microsoft Visual Studio\Installer\vswhere.exe" -utf8 -property installationPath -latest
+$vsPath = & "${env:ProgramFiles(x86)}\Microsoft Visual Studio\Installer\vswhere.exe" -utf8 -property installationPath -version '[17,99]'
+if (!$vsPath) {
+    exit 1
+}
+
 Import-Module "${vsPath}\Common7\Tools\Microsoft.VisualStudio.DevShell.dll"
 Enter-VsDevShell -VsInstallPath $vsPath -DevCmdArguments "-arch=amd64 -host_arch=amd64 -no_logo" -SkipAutomaticLocation
 
@@ -13,12 +17,20 @@ if (-Not (Test-Path $workingDir)) {
     Move-Item 'avs_core/include' '../include'
     Set-Location $workingDir
 
-    $latestRelease = Invoke-RestMethod -Headers @{ 'Accept' = 'application/vnd.github.v3+json' } 'https://api.github.com/repos/AviSynth/AviSynthPlus/releases/latest'
-    foreach ($asset in $latestRelease.assets) {
-        if ($asset.name -like 'AviSynthPlus_*-filesonly.7z') {
-            Invoke-WebRequest $asset.browser_download_url -OutFile $asset.name
-            7z e $asset.name -olibs\32 '*\x86\c_api\*.lib'
-            7z e $asset.name -olibs\64 '*\x64\c_api\*.lib'
+    $releases = Invoke-RestMethod -Headers @{ 'Accept' = 'application/vnd.github.v3+json' } 'https://api.github.com/repos/AviSynth/AviSynthPlus/releases'
+    $foundAsset = $false
+    foreach ($rel in $releases) {
+        foreach ($asset in $rel.assets) {
+            if ($asset.name -like 'AviSynthPlus_*-filesonly.7z') {
+                Invoke-WebRequest $asset.browser_download_url -OutFile $asset.name
+                7z e $asset.name -olibs\32 '*\x86\c_api\*.lib'
+                7z e $asset.name -olibs\64 '*\x64\c_api\*.lib'
+                $foundAsset = $true
+                break
+            }
+        }
+
+        if ($foundAsset) {
             break
         }
     }
@@ -31,13 +43,21 @@ if (-Not (Test-Path $workingDir)) {
     mkdir $workingDir
     Set-Location $workingDir
 
-    $latestRelease = Invoke-RestMethod -Headers @{ 'Accept' = 'application/vnd.github.v3+json' } 'https://api.github.com/repos/vapoursynth/vapoursynth/releases/latest'
-    foreach ($asset in $latestRelease.assets) {
-        if ($asset.name -like 'VapourSynth64-Portable-R*.7z') {
-            Invoke-WebRequest $asset.browser_download_url -OutFile $asset.name
-            7z e $asset.name -oinclude 'sdk\include\*'
-            7z e $asset.name -olibs\32 'sdk\lib32\*'
-            7z e $asset.name -olibs\64 'sdk\lib64\*'
+    $latestRelease = Invoke-RestMethod -Headers @{ 'Accept' = 'application/vnd.github.v3+json' } 'https://api.github.com/repos/vapoursynth/vapoursynth/releases'
+    $foundAsset = $false
+    foreach ($rel in $releases) {
+        foreach ($asset in $latestRelease.assets) {
+            if ($asset.name -like 'VapourSynth64-Portable-R*.7z') {
+                Invoke-WebRequest $asset.browser_download_url -OutFile $asset.name
+                7z e $asset.name -oinclude 'sdk\include\*'
+                7z e $asset.name -olibs\32 'sdk\lib32\*'
+                7z e $asset.name -olibs\64 'sdk\lib64\*'
+                $foundAsset = $true
+                break
+            }
+        }
+
+        if ($foundAsset) {
             break
         }
     }
