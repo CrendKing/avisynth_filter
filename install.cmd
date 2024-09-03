706bc8d818ba132546a9a5f56da09da3a3acdfa7
Crend King <975235+CrendKing@users.noreply.github.com>
2024-09-03 10:40:45 -0700

    Remove obsolete references to vapoursynth_filter_32.ax
    Fix issue #95

--- install.cmd
+++ install.cmd
@@ -1,4 +1,3 @@
 regsvr32 "%~dp0avisynth_filter_64.ax"
 regsvr32 "%~dp0avisynth_filter_32.ax"
 regsvr32 "%~dp0vapoursynth_filter_64.ax"
-regsvr32 "%~dp0vapoursynth_filter_32.ax"
--- uninstall.cmd
+++ uninstall.cmd
@@ -1,5 +1,4 @@
 regsvr32 /u "%~dp0avisynth_filter_64.ax"
 regsvr32 /u "%~dp0avisynth_filter_32.ax"
 regsvr32 /u "%~dp0vapoursynth_filter_64.ax"
-regsvr32 /u "%~dp0vapoursynth_filter_32.ax"
 start /min reg delete HKEY_CURRENT_USER\Software\AviSynthFilter /f
